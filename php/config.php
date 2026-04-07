d81312edfe056705d1b431d197ccf9ac935c2642
Swarupa Kumari <swarupakumari126@gmail.com>
2026-04-07 17:57:52 +0530

    php config

--- php/config.php
+++ php/config.php
@@ -1,7 +1,13 @@
 <?php
-$conn = new mysqli("localhost", "root", "", "user_auth", 3307);
+$conn = new mysqli(
+    getenv("MYSQLHOST"),
+    getenv("MYSQLUSER"),
+    getenv("MYSQLPASSWORD"),
+    getenv("MYSQLDATABASE"),
+    getenv("MYSQLPORT")
+);
 
 if ($conn->connect_error) {
-    die("Connection failed");
+    die("Connection failed: " . $conn->connect_error);
 }
 ?>
